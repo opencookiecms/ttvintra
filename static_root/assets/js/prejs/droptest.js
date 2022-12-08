73d6b51ef9275061479f3d9ef782e7fe6c107c55
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2022-12-08 14:41:09 +0800

    update

--- static_root/assets/js/prejs/droptest.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function ShowHideDiv() {
-    var chkYes = document.getElementById("chkYes");
-    var lug = document.getElementById("lug");
-    lug.style.display = chkYes.checked ? "block" : "none";
-}
