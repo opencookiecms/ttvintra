12f46c5fea539c0f7b6c189c86b05f17d8c4489d
SYED MOHD AFIQ SYED ABDUL RAHMAN <syed.m.afiq@outlook.com>
2022-12-08 13:29:42 +0800

    update

--- src/intra/settings.py
+++ src/intra/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'homebase',
     'trvrequest',
     'itasset',
+    'labassets',
 ]
 
 MIDDLEWARE = [
--- src/labassets/models.py
+++ src/labassets/models.py
@@ -13,8 +13,8 @@ class Camera(models.Model):
     mountingtype = models.CharField(max_length=50, null=True, blank=True) 
     interfacetype = models.CharField(max_length=50, null=True, blank=True) 
     
-    itempic = models.CharField(max_length=50, null=True, blank=True) 
-    attachment = models.CharField(max_length=50, null=True, blank=True) 
+    itempic = models.ImageField(null=True, blank=True) 
+    attachment = models.FileField(null=True, blank=True) 
     status  = models.CharField(max_length=50, null=True, blank=True)
     createby = models.CharField(max_length=150, null=True, blank=True)
     createdat = models.DateTimeField(auto_now_add=True, editable=False)
@@ -24,12 +24,12 @@ class Lens(models.Model):
     lensemodule  = models.CharField(max_length=100, null=True, blank=True) 
     assetno = models.CharField(max_length=50, null=True, blank=True) 
     location = models.CharField(max_length=50, null=True, blank=True)
-    typeitem
-    focallenth
-    mountingtype
+    typeitem = models.CharField(max_length=50, null=True, blank=True) 
+    focallenth = models.CharField(max_length=50, null=True, blank=True) 
+    mountingtype = models.CharField(max_length=50, null=True, blank=True) 
     itemdesc = models.TextField(null=True, blank=True) 
-    itempic = models.CharField(max_length=50, null=True, blank=True) 
-    attachment = models.CharField(max_length=50, null=True, blank=True)
+    itempic = models.ImageField(null=True, blank=True) 
+    attachment = models.FileField(null=True, blank=True) 
     status  = models.CharField(max_length=50, null=True, blank=True)
     createby = models.CharField(max_length=150, null=True, blank=True)
     createdat = models.DateTimeField(auto_now_add=True, editable=False)
@@ -41,8 +41,8 @@ class LightingCon(models.Model):
     assetno  = models.CharField(max_length=50, null=True, blank=True) 
     location = models.CharField(max_length=50, null=True, blank=True)
     itemdesc = models.TextField(null=True, blank=True) 
-    itempic = models.CharField(max_length=50, null=True, blank=True) 
-    attachment = models.CharField(max_length=50, null=True, blank=True)
+    itempic = models.ImageField(null=True, blank=True) 
+    attachment = models.FileField(null=True, blank=True) 
     status  = models.CharField(max_length=50, null=True, blank=True)
     createby = models.CharField(max_length=150, null=True, blank=True)
     createdat = models.DateTimeField(auto_now_add=True, editable=False)
@@ -53,16 +53,16 @@ class Lighting(models.Model):
     lightingmodul  = models.CharField(max_length=100, null=True, blank=True) 
     assetno = models.CharField(max_length=50, null=True, blank=True) 
     location = models.CharField(max_length=50, null=True, blank=True)
-    typeitem
-    coloritem
-    wattage
-    voltage
-    currentamp
-    manufacturing
-    itempic
-    attachment
-    quantity
-    status
+    typeitem = models.CharField(max_length=50, null=True, blank=True) 
+    coloritem = models.CharField(max_length=50, null=True, blank=True) 
+    wattage = models.CharField(max_length=50, null=True, blank=True) 
+    voltage = models.CharField(max_length=50, null=True, blank=True) 
+    currentamp = models.CharField(max_length=50, null=True, blank=True) 
+    manufacturing = models.CharField(max_length=50, null=True, blank=True) 
+    itempic = models.ImageField(null=True, blank=True) 
+    attachment = models.FileField(null=True, blank=True) 
+    quantity = models.CharField(max_length=50, null=True, blank=True) 
+    status = models.CharField(max_length=50, null=True, blank=True) 
     itemdesc = models.TextField(null=True, blank=True) 
     createby = models.CharField(max_length=150, null=True, blank=True)
     createdat = models.DateTimeField(auto_now_add=True, editable=False)
@@ -73,9 +73,9 @@ class Laser(models.Model):
     assetno  = models.CharField(max_length=50, null=True, blank=True) 
     location = models.CharField(max_length=50, null=True, blank=True)
     itemdesc = models.TextField(null=True, blank=True)  
-    attachment
-    itempic
-    status
+    itempic = models.ImageField(null=True, blank=True) 
+    attachment = models.FileField(null=True, blank=True) 
+    status = models.CharField(max_length=50, null=True, blank=True) 
     createby = models.CharField(max_length=150, null=True, blank=True)
     createdat = models.DateTimeField(auto_now_add=True, editable=False)
     updatedat = models.DateTimeField(auto_now=True, editable=False)
@@ -84,78 +84,83 @@ class Powersupply(models.Model):
     powermodule  = models.CharField(max_length=100, null=True, blank=True) 
     assetno  = models.CharField(max_length=50, null=True, blank=True) 
     location  = models.CharField(max_length=50, null=True, blank=True)
-    voltage 
+    voltage  = models.CharField(max_length=50, null=True, blank=True) 
     itemdesc = models.TextField(null=True, blank=True)  
-    attachement
-    itempic
-    status
+    itempic = models.ImageField(null=True, blank=True) 
+    attachment = models.FileField(null=True, blank=True) 
+    status = models.CharField(max_length=50, null=True, blank=True) 
     createby = models.CharField(max_length=150, null=True, blank=True)
     createdat = models.DateTimeField(auto_now_add=True, editable=False)
     updatedat = models.DateTimeField(auto_now=True, editable=False)
+    quantity  = models.IntegerField(null=True, blank=True) 
 
 class Cable(models.Model):
     cablemodule  = models.CharField(max_length=100, null=True, blank=True) 
     assetsno = models.CharField(max_length=50, null=True, blank=True)  
     location = models.CharField(max_length=50, null=True, blank=True)
     itemdesc = models.TextField(null=True, blank=True) 
-    quantity 
-    quantityaval
-    attachment
-    itempic 
-    status
+    quantity  = models.IntegerField(null=True, blank=True) 
+    itempic = models.ImageField(null=True, blank=True) 
+    attachment = models.FileField(null=True, blank=True)  
+    status = models.CharField(max_length=50, null=True, blank=True) 
     createby = models.CharField(max_length=150, null=True, blank=True)
     createdat = models.DateTimeField(auto_now_add=True, editable=False)
     updatedat = models.DateTimeField(auto_now=True, editable=False)
+    quantity  = models.IntegerField(null=True, blank=True) 
 
 class Card(models.Model):
     cardmodule  = models.CharField(max_length=100, null=True, blank=True) 
     assetno = models.CharField(max_length=50, null=True, blank=True)  
     location  = models.CharField(max_length=50, null=True, blank=True)
-    itemtype 
-    attachment
-    itempic 
-    status
+    itemtype  = models.CharField(max_length=50, null=True, blank=True) 
+    itempic = models.ImageField(null=True, blank=True) 
+    attachment = models.FileField(null=True, blank=True)  
+    status = models.CharField(max_length=50, null=True, blank=True) 
     itemdesc = models.TextField(null=True, blank=True) 
     createby = models.CharField(max_length=150, null=True, blank=True)
     createdat = models.DateTimeField(auto_now_add=True, editable=False)
     updatedat = models.DateTimeField(auto_now=True, editable=False)
+    quantity  = models.IntegerField(null=True, blank=True) 
 
 class Caltarget(models.Model):
     calmodule  = models.CharField(max_length=100, null=True, blank=True) 
     assetno = models.CharField(max_length=50, null=True, blank=True)  
     location = models.CharField(max_length=50, null=True, blank=True)
-    itempic
-    attachment 
-    status
+    itempic = models.ImageField(null=True, blank=True) 
+    attachment = models.FileField(null=True, blank=True) 
+    status = models.CharField(max_length=50, null=True, blank=True) 
     itemdesc = models.TextField(null=True, blank=True) 
     createby = models.CharField(max_length=150, null=True, blank=True)
     createdat = models.DateTimeField(auto_now_add=True, editable=False)
     updatedat = models.DateTimeField(auto_now=True, editable=False)
+    quantity  = models.IntegerField(null=True, blank=True) 
 
 class Optic(models.Model):
     opticmodule  = models.CharField(max_length=100, null=True, blank=True) 
     assetno = models.CharField(max_length=50, null=True, blank=True)  
     location  = models.CharField(max_length=50, null=True, blank=True)
-    itemtype
-    attachment
+    itemtype = models.CharField(max_length=50, null=True, blank=True) 
     itemdesc = models.TextField(null=True, blank=True) 
-    itempic
-    status
+    itempic = models.ImageField(null=True, blank=True) 
+    attachment = models.FileField(null=True, blank=True) 
+    status = models.CharField(max_length=50, null=True, blank=True) 
     createby = models.CharField(max_length=150, null=True, blank=True)
     createdat = models.DateTimeField(auto_now_add=True, editable=False)
     updatedat = models.DateTimeField(auto_now=True, editable=False)
+    quantity  = models.IntegerField(null=True, blank=True) 
 
 class Misc(models.Model):
     miscmodule  = models.CharField(max_length=100, null=True, blank=True) 
     assetno = models.CharField(max_length=50, null=True, blank=True) 
     itemdesc = models.TextField(null=True, blank=True)  
     location = models.CharField(max_length=50, null=True, blank=True)
-    itempic
-    attachment 
-    status
+    itempic = models.ImageField(null=True, blank=True) 
+    attachment = models.FileField(null=True, blank=True) 
+    status = models.CharField(max_length=50, null=True, blank=True) 
     createby = models.CharField(max_length=150, null=True, blank=True)
     createdat = models.DateTimeField(auto_now_add=True, editable=False)
     updatedat = models.DateTimeField(auto_now=True, editable=False)
+    quantity  = models.IntegerField(null=True, blank=True) 
 
     
 
